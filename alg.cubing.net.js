6262a60dc554785eba89db622c33eb31235e08a7
kirahan <hanzhaoapply@gmail.com>
2015-09-01 19:43:32 +0800

    增加输入按钮

--- alg.cubing.net.js
+++ alg.cubing.net.js
@@ -245,6 +245,62 @@ algxControllers.controller('algxController', ["$scope", "$location", "debounce",
       $("#canvasPNG").fadeTo("slow", 1);
   }
 
+
+  //在textarea中插入文本
+  $.fn.insertContent = function(myValue, t) {
+
+    var $t = $(this)[0];
+
+    if (document.selection) { //ie
+      this.focus();
+      var sel = document.selection.createRange();
+      sel.text = myValue;
+      this.focus();
+      sel.moveStart('character', -l);
+      var wee = sel.text.length;
+      if (arguments.length == 2) {
+        var l = $t.value.length;
+        sel.moveEnd("character", wee + t);
+        t <= 0 ? sel.moveStart("character", wee - 2 * t - myValue.length) : sel.moveStart("character", wee - t - myValue.length);
+
+        sel.select();
+      }
+    } else if ($t.selectionStart || $t.selectionStart == '0') {
+      var startPos = $t.selectionStart;
+
+      var endPos = $t.selectionEnd;
+      var scrollTop = $t.scrollTop;
+      $t.value = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);
+      this.focus();
+      $t.selectionStart = startPos + myValue.length;
+      $t.selectionEnd = startPos + myValue.length;
+      $t.scrollTop = scrollTop;
+      if (arguments.length == 2) {
+
+        $t.setSelectionRange(startPos - t, $t.selectionEnd + t);
+        this.focus();
+      }
+    }
+    else {
+      this.value += myValue;
+      this.focus();
+    };
+  }
+
+  $scope.insert_string_into_textarea = function (textarea,string) {
+    $("#"+textarea).insertContent(string);
+    $("#"+textarea).focus();
+    //var alg_value = "{alg:" + $("#"+textarea).value  +"}" ;
+    //for (i in alg_value) {
+    //  $scope[i] = alg_value[i];
+    //}
+  }
+
+  //function insert_string_into_textarea (textarea,string) {
+  //  $("#"+textarea).insertContent(string);
+  //}
+
+
   //$scope.gif = function() {
   //  //    var gif = new GIF({
   //    //  workers: 2,
